f84f2899794e8922cef61ab8cb46c0ace1b3a8f5
nady <naadydev>
2024-06-15 23:00:07 +0200

    add table

---
+++ people.jsx
@@ -0,0 +1,101 @@
+const foundations = [
+    {
+        "displayName": "Wolfgang",
+        "gitHubUserName": "wolfgangbecker",
+        "avatar_url": "url",
+        "atlassianAccountId": "5b2c4b00d2f5b64e4d21afe4"
+    },
+    {
+        "displayName": "David Mora",
+        "gitHubUserName": "davidgm0",
+        "avatar_url": "url",
+        "atlassianAccountId": "606242ceaee24000685b8fb1"
+    },
+    {
+        "displayName": "Mohit Yadav",
+        "gitHubUserName": "mohitkyadav",
+        "avatar_url": "url",
+        "atlassianAccountId": "6243931df813eb006928eaea"
+    },
+    {
+        "displayName": "Mahmoud Adly",
+        "gitHubUserName": "mahmoudadly-cobalt",
+        "avatar_url": "url",
+        "atlassianAccountId": "5fecb354d36496013986e885"
+    },
+    {
+        "displayName": "Taylor Briggs",
+        "gitHubUserName": "TaylorBriggs",
+        "avatar_url": "url",
+        "atlassianAccountId": "62e2aa503aaeedcae7566cce"
+    },
+    {
+        "displayName": "Grayson Kuhns",
+        "gitHubUserName": "grayson-cobalt",
+        "avatar_url": "url",
+        "atlassianAccountId": "622293504160640069c7eac3"
+    },
+    {
+        "displayName": "Akos Kitta",
+        "gitHubUserName": "kittaakos",
+        "avatar_url": "url",
+        "atlassianAccountId": "712020:7540217d-9c82-4bee-a37c-68ee0169cb23"
+    }
+];
+
+const ReginaTeams = [
+    {
+        "displayName": "Josh Hamacher",
+        "gitHubUserName": "josh-hamacher-cobalt",
+        "avatar_url": "url",
+        "atlassianAccountId": "607076fd2b469c0070692eba"
+    },
+    {
+        "displayName": "Amr Taha",
+        "gitHubUserName": "amrdruid",
+        "avatar_url": "url",
+        "atlassianAccountId": "712020:811b81f8-aaf0-40d3-be4d-f2a942233fbf"
+    },
+    {
+        "displayName": "Laura Kenny",
+        "gitHubUserName": "laura-kenny",
+        "avatar_url": "url",
+        "atlassianAccountId": "61d78ec17c6f9800706a4875"
+    },
+    {
+        "displayName": "Marijana Mustra",
+        "gitHubUserName": "marijanamustra",
+        "avatar_url": "url",
+        "atlassianAccountId": "608c5b94543264006892ef83"
+    },
+    {
+        "displayName": "Mauricio Reis",
+        "gitHubUserName": "mauricio-reis",
+        "avatar_url": "url",
+        "atlassianAccountId": "60647f859bc49b0070ec21f7"
+    },
+    {
+        "displayName": "Max Medina",
+        "gitHubUserName": "cobalt-medma",
+        "avatar_url": "url",
+        "atlassianAccountId": "6001dcf791bb2e0108407a64"
+    },
+    {
+        "displayName": "Nicolas Heinig",
+        "gitHubUserName": "nicolas-heinig",
+        "avatar_url": "url",
+        "atlassianAccountId": "712020:03b63236-858b-434b-a060-42f5ebc1d792"
+    },
+    {
+        "displayName": "Paul",
+        "gitHubUserName": "Lucianolo",
+        "avatar_url": "url",
+        "atlassianAccountId": "5e37f91fb341530e62acd339"
+    },
+    {
+        "displayName": "Roger Pfautz",
+        "gitHubUserName": "roger-cobalt",
+        "avatar_url": "url",
+        "atlassianAccountId": "628c1591ab963e006fe78f90"
+    }
+];
--- src/frontend/components/Config.jsx
+++ src/frontend/components/Config.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
 import { Textfield } from '@forge/react';
 
-const exampleUsersListString = '[{"gitHubUserName":"wolfgangbecker","displayName":"Wolfgang","avatar_url":"url","atlassianAccountId":"5b2c4b00d2f5b64e4d21afe4"},{"gitHubUserName":"davidgm0","displayName":"David Mora","avatar_url":"url","atlassianAccountId":"606242ceaee24000685b8fb1"},{"gitHubUserName":"mohitkyadav","displayName":"Mohit Yadav","avatar_url":"url","atlassianAccountId":"6243931df813eb006928eaea"}]';
+const exampleUsersListString = `[
+  {
+    "displayName": "Wolfgang",
+    "gitHubUserName": "wolfgangbecker",
+    "avatar_url": "url",
+    "atlassianAccountId": "5b2c4b00d2f5b64e4d21afe4"
+  },
+  {
+    "displayName": "David Mora",
+    "gitHubUserName": "davidgm0",
+    "avatar_url": "url",
+    "atlassianAccountId": "606242ceaee24000685b8fb1"
+  },
+  {
+    "displayName": "Mohit Yadav",
+    "gitHubUserName": "mohitkyadav",
+    "avatar_url": "url",
+    "atlassianAccountId": "6243931df813eb006928eaea"
+  },
+  {
+    "displayName": "Mahmoud Adly",
+    "gitHubUserName": "mahmoudadly-cobalt",
+    "avatar_url": "url",
+    "atlassianAccountId": "5fecb354d36496013986e885"
+  },
+  {
+    "displayName": "Taylor Briggs",
+    "gitHubUserName": "TaylorBriggs",
+    "avatar_url": "url",
+    "atlassianAccountId": "62e2aa503aaeedcae7566cce"
+  },
+  {
+    "displayName": "Grayson Kuhns",
+    "gitHubUserName": "grayson-cobalt",
+    "avatar_url": "url",
+    "atlassianAccountId": "622293504160640069c7eac3"
+  },
+  {
+    "displayName": "Akos Kitta",
+    "gitHubUserName": "kittaakos",
+    "avatar_url": "url",
+    "atlassianAccountId": "712020:7540217d-9c82-4bee-a37c-68ee0169cb23"
+  }
+]`;
 const exampleReposListString = '["cobalt-pentest-api", "cobalt-app-web"]';
 
 export const defaultConfig = {
